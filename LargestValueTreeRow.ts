8c8464bcdd70a4adaadc5e5064bcdeff2c9e24a9
swarajrath <gulal009@gmail.com>
2022-05-08 17:24:46 +0200

    initial commit

---
+++ LargestValueTreeRow.ts
@@ -0,0 +1,13 @@
+class treeNode<T> {
+    constructor(data:number, left:number = null, right:number = null){
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class BST {
+    constructor(){
+        this.root = null;
+    }
+}
